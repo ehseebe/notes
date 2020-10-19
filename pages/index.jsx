6dcf903d5d69191581b283dda2d99fa6cbd91bb3
Alyssa Beaton Tanguay <ehseebe@gmail.com>
2020-10-19 16:12:22 -0400

    Saving to localStorage

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,33 +1,50 @@
-import { useState } from 'react';
-import produce from 'immer';
+import { useState, useEffect } from "react";
+import produce from "immer";
 
-const Notes = props => props.data.map(note => <div>{note.text}</div>);
+const Notes = (props) => props.data.map((note) => <div>{note.text}</div>);
 
-export default () => {
-
-  const initialData = [{text: "hiho"}, {text: "howdy-do"}];
+export default function NoteApp() {
+  const initialData = [{ text: "Loading..." }];
 
   const [data, setData] = useState(initialData);
 
   const handleClick = () => {
-    const text = document.querySelector('#noteinput').value.trim();
+    const text = document.querySelector("#noteinput").value.trim();
     if (text) {
-      const nextState = produce(data, draftState => {
+      const nextState = produce(data, (draftState) => {
         draftState.push({ text });
       });
-      document.querySelector('#noteinput').value = '';
+      document.querySelector("#noteinput").value = "";
+
+      if (typeof window !== "undefined") {
+        localStorage.setItem("data", JSON.stringify(nextState));
+      }
+
       setData(nextState);
     }
-  }
+  };
 
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const getNotes = localStorage.getItem("notes");
+      if (getNotes !== "" && getNotes !== null) {
+        return setData(JSON.parse(getNotes));
+      }
+      return setData([]);
+    }
+  }, []);
 
   return (
     <>
+      <input
+        id="noteinput"
+        style={{ width: "80%" }}
+        type="text"
+        placeholder="Enter new note"
+      />
 
-    <input id="noteinput" style={{ width: '80%' }} type="text" placeholder="Enter new note" />
-
-    <button onClick={() => handleClick()}> Add note </button>
-  <Notes data={data} />
-  </>
-  )
+      <button onClick={() => handleClick()}> Add note </button>
+      <Notes data={data} />
+    </>
+  );
 }
